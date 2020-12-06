12920fb53cc874a12ff4c957645a2fefd883b1f0
lindeluhuang <linde.lu.huang@gmail.com>
2020-12-05 19:15:43 -0500

    switching content on sections

--- js-experiment-4/js/scroll.js
+++ js-experiment-4/js/scroll.js
@@ -2,6 +2,10 @@ const pixelsTag = document.querySelector("div.pixels")
 const bodyTag = document.querySelector("body")
 const progressTag = document.querySelector("div.progress")
 
+const sections = document.querySelectorAll("section")
+const typeTag = document.querySelector("div.type")
+const pageTag = document.querySelector("div.page")
+
 // when scrolling page, update pixels tag into how far scrolled
 document.addEventListener("scroll", function() {
     const pixels = window.pageYOffset;
@@ -16,4 +20,18 @@ document.addEventListener("scroll", function() {
     const percentScrolled = pixels / totalScrollableDistance * 100;
 
     progressTag.style.width = percentScrolled + "%";
+})
+
+// when we scroll, see how far down we've scrolled
+// then for each section, change if passed
+document.addEventListener("scroll", function() {
+    const pixels = window.pageYOffset;
+
+    sections.forEach(section => {
+        if (pixels >= section.offsetTop - 60) {
+            typeTag.innerHTML = section.getAttribute("data-type")
+            pageTag.innerHTML = section.getAttribute("data-page")
+        }
+    })
+
 })
